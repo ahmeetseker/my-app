fbc15a1412c0e143284d754dc9867436dd0cf226
ahmet <ahmetseker1905@hotmail.com.tr>
2024-01-09 21:59:59 +0300

    deploytry4

--- src/assets/components/LanguageSelectorMobile.jsx
+++ src/assets/components/LanguageSelectorMobile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import TR from '../../assets/tr.svg'
-import EN from '../../assets/EN.svg'
+import EN from '../../assets/en.svg'
 
 
 function LanguageSelector() {
